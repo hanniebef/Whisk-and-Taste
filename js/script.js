3e7520c0ba256347ffcadb8bb4d1f558989cc957
Hannie <hbefeler@gmail.com>
2024-11-07 12:25:34 +0100

    Responsive DONE

--- js/script.js
+++ js/script.js
@@ -1,34 +1,40 @@
+// AOS
   AOS.init();
 
-  $(document).ready(function(){
-    $('.center').slick({
-      centerMode: true,
-      centerPadding: '60px',
-      slidesToShow: 3,
-      responsive: [
-        {
-          breakpoint: 768,
-          settings: {
-            arrows: false,
-            centerMode: true,
-            centerPadding: '40px',
-            slidesToShow: 3
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            arrows: false,
-            centerMode: true,
-            centerPadding: '40px',
-            slidesToShow: 1
-          }
-        }
-      ]
-    });
-  });
 
-  // NAVBAR
+// CARRUSEL
+
+$(document).ready(function(){
+  $('.center').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    arrows: true, 
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: true,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: true,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+});
+
+
+// NAVBAR
 
 document.addEventListener('DOMContentLoaded', function () {
 
